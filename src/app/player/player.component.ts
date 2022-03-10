c541faa1ca3a32ee11952818ea4fa3e2ce2e1de5
Dominic Gerth <d.gerth1990@googlemail.com>
2022-03-10 14:46:03 +0100

    imlplemented players from string

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-player',
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+
+  @Input() name: any;
 
   ngOnInit(): void {
   }
